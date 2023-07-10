aa8a60a89ff042a84d68777b4e1caec7055aab4f
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-07-10 18:52:08 +0000

    resource

--- src/main/java/com/example/demo/CursoRepository.java
+++ src/main/java/com/example/demo/CursoRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
-public interface CursoRepository extends CrudRepository<Curso,Integer>{
+@RepositoryRestResource(collectionResourceRel = "cursos", path = "cursos")
+public interface CursoRepository extends PagingAndSortingRepository<Curso, Integer>,CrudRepository<Curso, Integer> {
     
+    List<Curso> findByNombre(@Param("nombre") String nombre);
+
 }
