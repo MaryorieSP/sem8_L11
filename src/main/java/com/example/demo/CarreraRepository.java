8195d495319ca55b149e19d308e46190a3022fc9
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-07-10 19:31:08 +0000

    carrera

--- src/main/java/com/example/demo/CarreraRepository.java
+++ src/main/java/com/example/demo/CarreraRepository.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
