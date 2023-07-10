cb7f9a6158093d1187c9d555f2b922e294f5020f
MaryorieSP <134321286+MaryorieSP@users.noreply.github.com>
2023-07-10 17:09:12 +0000

    semana 8

---
+++ src/main/java/com/example/demo/Curso.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "curso")
+public class Curso {
+    
+    @Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+    private String nombre;
+    private Integer creditos;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Integer getCreditos() {
+        return creditos;
+    }
+    public void setCreditos(Integer creditos) {
+        this.creditos = creditos;
+    }
+
+    
+}
